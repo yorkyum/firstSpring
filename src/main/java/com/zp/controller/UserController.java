749d59a3ec65363561ce384545c9b61ab7a6db52
york <york@york-OMEN-by-HP-Laptop-15-ce0xx>
2018-03-29 12:19:22 +0800

    fix bug

--- src/main/java/com/zp/controller/UserController.java
+++ src/main/java/com/zp/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.zp.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.zp.service.UserService;
+import com.zp.util.PageData;
+
 
 
 /**
@@ -12,12 +22,25 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value="user")
 public class UserController {
-	@RequestMapping(value="userLogin")
-	public ModelAndView talentSchemeList() throws Exception{
+	@Resource(name="userService")
+	private UserService userService;
+	
+	@RequestMapping(value="toUserList")
+	public ModelAndView toUserList() throws Exception{
+		List<PageData> pdList = userService.userList(null);
 		
-		System.out.println("-------------------------");
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("success");
+		mv.addObject("pdList",pdList);
+		mv.getModel().put("name", "aaaaaa");
+		mv.setViewName("login/userList");
 		return mv;
+	}	
+	@RequestMapping(value="userLogin")
+	@ResponseBody
+	public PageData userLogin(HttpServletRequest request) throws Exception{
+		PageData pd = new PageData(request);
+	  	pd.put("msg", "登录成功！");
+	  	toUserList();
+	  	return pd;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,31 @@
 <%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
 <html>
 <body>
-<h2>Hello World222!</h2>
-<a href="user/userLogin">用户登录</a>
+<h2>Hello World!</h2>
+<a onclick="login('user/userLogin','11','22')" href="#">用户登录1</a>
+<a  href="user/toUserList">用户登录2</a>
 </body>
 </html>
+<script type="text/javascript" src="<%=basePath %>js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript">
+	function login(url,userName,passWord){
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : {
+				userName : userName,
+				passWord : passWord
+			},
+			dataType : "json",
+			async : false,
+			success : function(data) {
+				alert(data.msg);
+			}
+		});
+	}
+</script>
